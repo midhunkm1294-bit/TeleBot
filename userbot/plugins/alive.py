5bf909a49b75d0cb45550d5e072a9e49385ff517
Midhun K M <midhunkm1294.stark@gmail.com>
2020-05-22 18:43:31 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,10 +11,22 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("   **Welcome To TeleBot ** \n\n"
+    await alive.edit("   **
+
+███████╗██████╗░██╗██████╗░░█████╗░██╗░░░██╗
+
+██╔════╝██╔══██╗██║██╔══██╗██╔══██╗╚██╗░██╔╝
+
+█████╗░░██████╔╝██║██║░░██║███████║░╚████╔╝░
+
+██╔══╝░░██╔══██╗██║██║░░██║██╔══██║░░╚██╔╝░░
+
+██║░░░░░██║░░██║██║██████╔╝██║░░██║░░░██║░░░
+
+╚═╝░░░░░╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░ ** \n\n"
         "**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
                       "` 🔸 Telethon version:` **6.9.0**\n` 🔹 Python:` **3.7.3** \n` 🔸 More info:` @TeleBotHelpChat\n"
-                     "` 🔹 Bot created by:` [Aditya 🇮🇳](tg://user?id=719195224)\n"
+                     "` 🔹 Bot created by:` [MarioDevs ](tg://user?id=813878981)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
-                     f"` 🔹 My peru owner`: {DEFAULTUSER}\n\n"
-                     "           [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)")
+                     f"` 🔹 My pro owner`: {DEFAULTUSER}\n\n"
+                     "           [✨ GitHub Repository ✨](https://github.com/midhunkm1294-bit/FRIDAY)")
